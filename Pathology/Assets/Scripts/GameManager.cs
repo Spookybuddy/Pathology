747085606484c65ae06fca80a72b591386f04c86
Spookybuddy <jliving3@student.fitchburgstate.edu>
2023-09-24 17:41:48 -0400

    Dialog display
    Dialog now appears letter by letter, and can be skipped by pressing a button. The player's options for responses are now displayed (Add click compatibility later). Setup for door & interior controls. Setup for items & inventory. Input delay & pausing fixed.

--- Pathology/Assets/Scripts/GameManager.cs
+++ Pathology/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,23 +9,7 @@ public class GameManager : MonoBehaviour
     public CharacterText currentConvo;
 
     //Player inputs are translated into whatever the current conversation is
-    public void Advance(int EastNorthWest)
-    {
-        currentConvo.PlayerContinue(EastNorthWest);
-    }
-
-    public void Decline()
-    {
-        currentConvo.PlayerCancel();
-    }
-
-    public void PlayerLeaves()
-    {
-        player.CloseDialog();
-    }
-
-    public void PlayerResponses(int amount)
-    {
-
-    }
+    public void Advance(int EastNorthWest) { currentConvo.PlayerContinue(EastNorthWest); }
+    public void Decline() { currentConvo.PlayerCancel(); }
+    public void PlayerLeaves() { player.CloseDialog(); }
 }
