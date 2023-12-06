d956b379643e5d1a240126b589f46b0606ee7071
Spookybuddy <93450263+Spookybuddy@users.noreply.github.com>
2023-12-06 17:52:26 -0500

    Lab work
    Transition on Title. Updated player prefab. UI mousing. Animation improvements. Bug Fixes

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -12,25 +12,30 @@ public class MainManager : MonoBehaviour
     private string filename;
     private string[] data;
     public string settings;
-    public int volume;
-    public bool clickMove;
+    private int volume;
+    private bool clickMove;
     private bool clickToggle;
-    public int txtSpd;
+    private int txtSpd;
 
     //Menu visuals
     public GameObject Title;
     public GameObject Options;
-    public bool menuUp;
+    private bool menuUp;
     public Slider volumeLvl;
     public Toggle toggle;
     public TextMeshProUGUI speed;
     private readonly string[] texts = new string[] { "Slow", "Normal", "Fast", "Instant" };
 
+    //Shader
+    public Material shader;
+    private float shaderValue;
+    private float transition;
+
     //Menu controls
     private Vector2 joystick;
     private Vector2 keyboard;
     private Vector2 dirpad;
-    public Vector2 _direction;
+    private Vector2 _direction;
     private bool inputting;
     private int horizontal;
     private int vertical;
@@ -39,10 +44,18 @@ public class MainManager : MonoBehaviour
     public GameObject[] MMO;
     public GameObject[] SM;
     public GameObject[] SMO;
+    public Vector2[] specialPadding;
     private float delay;
+    private Vector2 Mpos;
+    private readonly Vector2 padding = new Vector2(170, 50);
 
     void Awake()
     {
+        //Start with shader full
+        transition = 0.87f;
+        StartCoroutine(Shade(false));
+
+        //Read file data
         filename = Application.streamingAssetsPath + "/SaveData.txt";
         data = File.ReadAllLines(filename);
         settings = data[6];
@@ -53,6 +66,7 @@ public class MainManager : MonoBehaviour
     //Read in and set all the values from the save file
     void Start()
     {
+        if (!int.TryParse(settings, out int result)) settings = "100100";
         volume = int.Parse(settings.Substring(0, 3));
         clickMove = settings[3].Equals('1');
         txtSpd = int.Parse(settings.Substring(4, 1));
@@ -69,8 +83,8 @@ public class MainManager : MonoBehaviour
         _direction = VectorGreater(_direction, dirpad);
         if (_direction.magnitude > 0.167f) inputting = true;
         delay = Mathf.Clamp01(delay - Time.deltaTime);
+        UIOverlays();
         if (inputting) {
-            UIOverlays();
             if (delay == 0) {
                 if (menuUp) {
                     vertical = (vertical - (int)(sensitivity * _direction.y) + MM.Length) % MM.Length;
@@ -104,7 +118,25 @@ public class MainManager : MonoBehaviour
                 inputting = false;
             }
         }
-        //outline.transform.localPosition = (menuUp ? MM[vertical].transform.localPosition : SM[vertical].transform.localPosition);
+        
+        //Mouse over
+        if (menuUp) {
+            for (int i = 0; i < MM.Length; i++) {
+                if (Mpos.x > MM[i].transform.position.x - padding.x && Mpos.x < MM[i].transform.position.x + padding.x) {
+                    if (Mpos.y > MM[i].transform.position.y - padding.y && Mpos.y < MM[i].transform.position.y + padding.y) {
+                        vertical = i;
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < SM.Length; i++) {
+                if (Mpos.x > SM[i].transform.position.x - specialPadding[i].x && Mpos.x < SM[i].transform.position.x + specialPadding[i].x) {
+                    if (Mpos.y > SM[i].transform.position.y - specialPadding[i].y && Mpos.y < SM[i].transform.position.y + specialPadding[i].y) {
+                        vertical = i;
+                    }
+                }
+            }
+        }
     }
 
     //Write the vales to the save data & file
@@ -130,6 +162,7 @@ public class MainManager : MonoBehaviour
     //Changes the menu displaying
     public void ChangeMenu()
     {
+        if (transition != 0) return;
         menuUp = !menuUp;
         Title.SetActive(menuUp);
         Options.SetActive(!menuUp);
@@ -170,6 +203,7 @@ public class MainManager : MonoBehaviour
     //Exit the game
     public void Quit()
     {
+        if (transition != 0) return;
         Save();
         Application.Quit();
     }
@@ -177,13 +211,33 @@ public class MainManager : MonoBehaviour
     //Load scene
     public void Load(string scene)
     {
+        if (transition != 0) return;
         StartCoroutine(ChangeScene(scene));
     }
 
+    //Change the scene with the tranisition shader
     private IEnumerator ChangeScene(string scene)
     {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
-        while (!asyncLoad.isDone) yield return null;
+        asyncLoad.allowSceneActivation = false;
+        transition = 0.001f;
+        StartCoroutine(Shade(true));
+        while (!asyncLoad.isDone) {
+            if (asyncLoad.progress >= 0.9f && transition >= 0.875f) asyncLoad.allowSceneActivation = true;
+            yield return null;
+        }
+    }
+
+    //Update the transition shader
+    private IEnumerator Shade(bool pos)
+    {
+        yield return new WaitForSeconds(Time.deltaTime);
+        if (0 < transition && transition < 0.875f) {
+            transition = Mathf.Clamp(transition + (pos ? Time.deltaTime : -Time.deltaTime), 0, 0.875f);
+            shaderValue = Mathf.Pow(transition * (10 / 0.875f), 2);
+            shader.SetFloat("_Scale", shaderValue);
+            StartCoroutine(Shade(pos));
+        }
     }
 
     //Returns greatest magnitude X Y of Vector2. This allows for dual controller use without overriding the other
@@ -252,5 +306,9 @@ public class MainManager : MonoBehaviour
         if (ctx.performed) {
             if (!menuUp) ChangeMenu();
         }
+    }
+    public void MousePosition(InputAction.CallbackContext ctx)
+    {
+        Mpos = ctx.ReadValue<Vector2>();
     }
 }
